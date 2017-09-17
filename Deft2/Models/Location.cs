c4c1130491576036d7f3c8324461bf8de7e7230b
Gabriel Barrett <gbarrett2@gmail.com>
2017-09-17 11:58:31 -0600

    tired of this

---
+++ Deft2/Models/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Deft2.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        public string LocationName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public bool ClosedSunday { get; set; }
+    }
+}
